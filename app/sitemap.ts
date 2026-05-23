bd5ba1c0e5caac67171192f89d3dff45fa555466
groovybuilds <groovybuilds@gmail.com>
2026-05-23 17:52:20 -0500

    Add FAQ page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,6 +19,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${siteUrl}/faq`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
       url: `${siteUrl}/careers`,
       lastModified,
       changeFrequency: "monthly",
